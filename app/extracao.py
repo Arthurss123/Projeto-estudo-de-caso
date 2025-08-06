7d37163a7690893f0a334917f35ae537bb5bf501
Arthur Moura <mouraarthur625@gmail.com>
2025-08-06 11:22:50 -0300

    Feat(extracao): Processar a pasta e função principal

--- app/extracao.py
+++ app/extracao.py
@@ -13,3 +13,19 @@ def extrair_texto(caminho_do_arquivo: str) -> str:
   except Exception as e:
     print("Erro no arquivo")
     return ""
+  
+def processar_pasta (caminho_da_pasta: str) -> dict:
+  textos_dos_documentos = {}
+  for nome_arquivo in os.listdir(caminho_da_pasta):
+    if nome_arquivo.lower().endswith(".pdf"):
+      caminho_completo = os.path.join(caminho_da_pasta, nome_arquivo)
+      print(f"Extraindo texto de: {nome_arquivo}")
+      textos_dos_documentos[nome_arquivo] = extrair_texto(caminho_completo)
+  return textos_dos_documentos
+
+if __name__ == '__main__':
+    dados_extraidos = processar_pasta("data")
+    for nome, texto in dados_extraidos.items():
+        print(f"--- Documento: {nome} ---")
+        print(f"{texto[:200]}...")
+        print("-" * 20)
